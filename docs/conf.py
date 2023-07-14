aea4230f67f1e44c52d6693eff99aa1406cf754a
Hamid <vakilzas@uww.edu>
2023-07-14 10:47:57 -0500

    Bump version: 0.0.1 → 0.0.2
    Took 3 minutes

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ project = "xbrl-us"
 year = "2023"
 author = "hamid-vakilzadeh"
 copyright = f"{year}, {author}"
-version = release = "0.0.1"
+version = release = "0.0.2"
 
 pygments_style = "emacs"
 highlight_language = "python"
