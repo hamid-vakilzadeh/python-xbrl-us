c86f1d86d317b66d3b8d307c4542daa29632cea3
Hamid Vakilzadeh <vakilzas@uww.edu>
2025-04-12 06:34:21 -0500

    Add initial types module with literals and typed dictionaries for XBRL processing

---
+++ src/xbrl_us/types/__init__.py
@@ -0,0 +1,7 @@
+from .types import FACT_ENDPOINTS
+from .types import FACT_FIELDS
+from .types import FACT_PARAMETERS
+from .types import FACT_SORT
+from .types import AcceptableMethods
+
+__all__ = ["AcceptableMethods", "FACT_SORT", "FACT_PARAMETERS", "FACT_FIELDS", "FACT_ENDPOINTS"]
