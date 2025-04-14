8caf83ca493ed9aa713456a8026cca5f10b300ab
Hamid Vakilzadeh <vakilzas@uww.edu>
2025-04-13 20:49:24 -0500

    Remove unused endpoint maps from __init__.py to streamline imports and improve code clarity

--- src/xbrl_us/types/__init__.py
+++ src/xbrl_us/types/__init__.py
@@ -1,143 +1,107 @@
 from .endpoints import AssertionEndpoint
-from .endpoints import AssertionEndpointMap
 from .endpoints import AssertionFields
 from .endpoints import AssertionParameters
 from .endpoints import ConceptEndpoint
-from .endpoints import ConceptEndpointMap
 from .endpoints import ConceptFields
 from .endpoints import ConceptParameters
 from .endpoints import CubeEndpoint
-from .endpoints import CubeEndpointMap
 from .endpoints import CubeFields
 from .endpoints import CubeParameters
 from .endpoints import DocumentEndpoint
-from .endpoints import DocumentEndpointMap
 from .endpoints import DocumentFields
 from .endpoints import DocumentParameters
 from .endpoints import DtsConceptEndpoint
-from .endpoints import DtsConceptEndpointMap
 from .endpoints import DtsConceptFields
 from .endpoints import DtsConceptParameters
 from .endpoints import DtsEndpoint
-from .endpoints import DtsEndpointMap
 from .endpoints import DtsFields
 from .endpoints import DtsNetworkEndpoint
-from .endpoints import DtsNetworkEndpointMap
 from .endpoints import DtsNetworkFields
 from .endpoints import DtsNetworkParameters
 from .endpoints import DtsParameters
 from .endpoints import EntityEndpoint
-from .endpoints import EntityEndpointMap
 from .endpoints import EntityFields
 from .endpoints import EntityParameters
 from .endpoints import EntityReportEndpoint
-from .endpoints import EntityReportEndpointMap
 from .endpoints import EntityReportFields
 from .endpoints import EntityReportParameters
 from .endpoints import FactEndpoint
-from .endpoints import FactEndpointMap
 from .endpoints import FactFields
 from .endpoints import FactParameters
 from .endpoints import LabelEndpoint
-from .endpoints import LabelEndpointMap
 from .endpoints import LabelFields
 from .endpoints import LabelParameters
 from .endpoints import NetworkEndpoint
-from .endpoints import NetworkEndpointMap
 from .endpoints import NetworkFields
 from .endpoints import NetworkParameters
 from .endpoints import NetworkRelationshipEndpoint
-from .endpoints import NetworkRelationshipEndpointMap
 from .endpoints import NetworkRelationshipFields
 from .endpoints import NetworkRelationshipParameters
 from .endpoints import RelationshipEndpoint
-from .endpoints import RelationshipEndpointMap
 from .endpoints import RelationshipFields
 from .endpoints import RelationshipParameters
 from .endpoints import ReportEndpoint
-from .endpoints import ReportEndpointMap
 from .endpoints import ReportFactEndpoint
-from .endpoints import ReportFactEndpointMap
 from .endpoints import ReportFactFields
 from .endpoints import ReportFactParameters
 from .endpoints import ReportFields
 from .endpoints import ReportNetworkEndpoint
-from .endpoints import ReportNetworkEndpointMap
 from .endpoints import ReportNetworkFields
 from .endpoints import ReportNetworkParameters
 from .endpoints import ReportParameters
-from .endpoints import UniversalFieldMap
 from .types import AcceptableMethods
 
 __all__ = [
     "AssertionEndpoint",
-    "AssertionEndpointMap",
     "AssertionFields",
     "AssertionParameters",
     "ConceptEndpoint",
-    "ConceptEndpointMap",
     "ConceptFields",
     "ConceptParameters",
     "CubeEndpoint",
-    "CubeEndpointMap",
     "CubeFields",
     "CubeParameters",
     "DocumentEndpoint",
-    "DocumentEndpointMap",
     "DocumentFields",
     "DocumentParameters",
     "DtsConceptEndpoint",
-    "DtsConceptEndpointMap",
     "DtsConceptFields",
     "DtsConceptParameters",
     "DtsEndpoint",
-    "DtsEndpointMap",
     "DtsFields",
     "DtsNetworkEndpoint",
-    "DtsNetworkEndpointMap",
     "DtsNetworkFields",
     "DtsNetworkParameters",
     "DtsParameters",
     "EntityEndpoint",
-    "EntityEndpointMap",
     "EntityFields",
     "EntityParameters",
     "EntityReportEndpoint",
-    "EntityReportEndpointMap",
     "EntityReportFields",
     "EntityReportParameters",
     "FactEndpoint",
-    "FactEndpointMap",
     "FactFields",
     "FactParameters",
     "LabelEndpoint",
-    "LabelEndpointMap",
     "LabelFields",
     "LabelParameters",
     "NetworkEndpoint",
-    "NetworkEndpointMap",
     "NetworkFields",
     "NetworkParameters",
     "NetworkRelationshipEndpoint",
-    "NetworkRelationshipEndpointMap",
     "NetworkRelationshipFields",
     "NetworkRelationshipParameters",
     "RelationshipEndpoint",
-    "RelationshipEndpointMap",
     "RelationshipFields",
     "RelationshipParameters",
     "ReportEndpoint",
-    "ReportEndpointMap",
     "ReportFactEndpoint",
-    "ReportFactEndpointMap",
     "ReportFactFields",
     "ReportFactParameters",
     "ReportFields",
     "ReportNetworkEndpoint",
-    "ReportNetworkEndpointMap",
     "ReportNetworkFields",
     "ReportNetworkParameters",
     "ReportParameters",
-    "UniversalFieldMap",
     "AcceptableMethods",
 ]
