4d63cdc0dab4ca143609e5dcdbb428b062e3eabb
Hamid Vakilzadeh <vakilzas@uww.edu>
2025-04-18 16:22:32 -0500

    Bump version: 0.1.0 → 1.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ project = "xbrl-us"
 year = "2023"
 author = "hamid-vakilzadeh"
 copyright = f"{year}, {author}"
-version = release = "0.1.0"
+version = release = "1.0.0"
 pygments_style = "emacs"
 highlight_language = "python"
 templates_path = ["."]
